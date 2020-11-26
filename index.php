f934e773a416b9b9b0214f6c3b730da5064b6a0a
davy1ex <dezy1ex@gmail.com>
2020-11-26 20:14:52 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="banner" style="background-image: url('img/banner.jpg')">
+        <header>
+            <div class="container">
+                <nav>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">Episodes</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Subscribe</a></li>
+                    </ul>
+                </nav>
+
+                <div class="donate">
+                    <div class="donate-img"><img src="icons/flash.png" alt=""></div>
+                    <div class="donate-text">Donate</div>
+                </div>
+            </div>
+        </header>
+
+        <!-- <div class="banner-desc container">
+            <div class="podcast-title">
+                The davycast podcast show
+            </div>
+
+            <div class="podcast-desc">
+                Richard Neil and his team talk about life, university and everything
+            </div>
+        </div>
+
+        <div class="podcast-teaser container">
+            <div class="podcast-img"><img src="img/microphone.jpg" alt=""></div>
+            
+            <div class="podcact-title">
+                davycast trailer
+            </div>
+
+            <div class="podcasts-data">
+                <div class="podcast-date">december 21, 2020</div>
+            </div>
+        </div> -->
+    </div>
+    
+    <!-- <div class="content container">
+        <div class="lastest-episodes">
+            <div class="title">latest episodes</div>
+
+            <div class="podcast">
+                <div class="podcast-img"><img src="img/microphone.jpg" alt=""></div>
+                
+                <div class="podcact-title">
+                    Ep. 007 What im do on Happy New Year
+                </div>
+
+                <div class="podcast-desc">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid consectetur labore natus. Ducimus recusandae adipisci libero iusto iure laudantium harum quas. Assumenda velit adipisci ut autem deleniti fuga. Fugit, vitae?
+                </div>
+
+                <div class="podcasts-data">
+                    <div class="podcast-date">december 21, 2020</div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="sidebar">
+            <div class="title">listen & subscribe</div>
+
+            <div class="socials">
+                <ul>
+                    <li><a href="#"><img src="icons/rss.png" alt=""> Apple Podcasts</a></li>
+                    <li><a href="#"><img src="icons/rss.png" alt=""> Google Podcasts</a></li>
+                    <li><a href="#"><img src="icons/rss.png" alt=""> Spotify</a></li>
+                    <li><a href="#"><img src="icons/rss.png" alt=""> Yandex.Musics</a></li>
+                </ul>
+            </div>
+        </div>
+    </div> -->
+
+    <script src="live.js"></script>
+</body>
+</html>
